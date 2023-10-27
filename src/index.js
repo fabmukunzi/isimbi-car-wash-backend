8b2759f63514eb649a31c0cd3d3765905ca3455a
leandre <tuyambazeleandre@gmail.com>
2023-10-27 09:21:33 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import routes from './routes';
-import fileUpload from 'express-fileupload';
-import passport from 'passport';
-import session from 'express-session';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import routes from "./routes";
+import { sequelize } from "./database/models";
+import fileUpload from "express-fileupload";
+import passport from "passport";
+import session from "express-session";
 
 dotenv.config();
 
@@ -12,14 +13,14 @@ const app = express();
 
 app.use(
   session({
-    secret: 'hello',
+    secret: "hello",
     resave: false,
     saveUninitialized: true,
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors())
+app.use(cors());
 app.use(
   fileUpload({
     useTempFiles: true,
@@ -27,15 +28,25 @@ app.use(
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', routes);
-app.get('*', (req, res) =>
+app.use("/", routes);
+app.get("*", (req, res) =>
   res.status(200).send({
-    message: 'Not Found',
+    message: "Not Found",
   })
 );
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log('App is now running at port ', port);
+  const connectDB = async () => {
+    try {
+      await sequelize.authenticate();
+      console.log("Database connection established successfully");
+    } catch (err) {
+      console.log(`Database connection failed: ${err}`);
+      process.exit(1);
+    }
+  };
+  connectDB();
+  console.log("App is now running at port ", port);
 });
